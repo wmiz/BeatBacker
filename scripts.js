9607da6e32d0c8d34ebadd2706b17f9da6303d4e
wmisback <will.misback@gmail.com>
2020-09-11 12:41:15 -0400

    Add extras section

--- scripts.js
+++ scripts.js
@@ -1,9 +1,22 @@
 // Populate measure boxes
 measures = document.querySelector("#measures");
+extras = document.querySelector("#extras");
+
+tempo = 60;
+
+let element;
 
 for (i=0;i<4;i++) {
-	let element = document.createElement("button")
-	element.innerHTML = i;
+	element = document.createElement("button")
+	element.innerHTML = i + 1;
 	element.classList.add("note-box");
 	measures.appendChild(element);
 }
+
+element = document.createElement("input")
+extras.appendChild(element);
+
+element = document.createElement("button")
+element.innerHTML = "START";
+extras.appendChild(element);
+
