f718c189122a0dbd2f1d1bb3dac693a70023c118
wmisback <will.misback@gmail.com>
2020-09-11 12:34:29 -0400

    Populate measure buttons

--- scripts.js
+++ scripts.js
@@ -1,1 +1,9 @@
-// Skeleton
+// Populate measure boxes
+measures = document.querySelector("#measures");
+
+for (i=0;i<4;i++) {
+	let element = document.createElement("button")
+	element.innerHTML = i;
+	element.classList.add("note-box");
+	measures.appendChild(element);
+}
