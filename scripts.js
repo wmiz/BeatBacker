4b40c2a30c1a07345280b19471ad19374f77f809
wmisback <will.misback@gmail.com>
2020-09-11 15:19:19 -0400

    Play sounds that have been selected once

--- scripts.js
+++ scripts.js
@@ -2,10 +2,10 @@ let sounds = ["Clap", "Hihat", "Kick", "Openhat", "Boom", "Ride", "Snare",
 "Tom", "Tink"]
 
 // Get parents
-measures = document.querySelector("#measures");
-extras = document.querySelector("#extras");
+const measures = document.querySelector("#measures");
+const extras = document.querySelector("#extras");
 
-tempo = 60;
+let tempo = 60;
 
 let element;
 
@@ -35,3 +35,27 @@ for (i=0;i<4;i++) {
 	outerDiv.appendChild(soundDiv);
 	measures.appendChild(outerDiv);
 }
+
+// Play sounds
+startButton = document.querySelector("#start-button");
+startButton.addEventListener("click", function() {
+	// Get note text
+	let notes = Array.from(measures.children);
+	let notesText = [];
+	notes.forEach(e => notesText.push(e.firstChild.innerHTML));
+	
+	// Get note text index
+	let notesIndices = [];
+	notesText.forEach(e => notesIndices.push(sounds.indexOf(`${e}`)));
+
+	// Play notes
+	let tempoMultiplier = tempo / 60;
+	let timeInterval = 1000 / tempoMultiplier;
+
+	soundsAudio = document.querySelectorAll("audio");
+	for (let i=0;i<notesIndices.length;i++) {
+		setTimeout(function() {
+			console.log(soundsAudio.item(notesIndices[i]).play());
+		}, timeInterval * i);
+	}
+});
