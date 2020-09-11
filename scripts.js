4bc4b4939e9e8c3ef061da34d7bd175c51ac3506
wmisback <will.misback@gmail.com>
2020-09-11 13:42:53 -0400

    Add working dropdowns and new color scheme

--- scripts.js
+++ scripts.js
@@ -1,4 +1,7 @@
-// Populate measure boxes
+let sounds = ["Clap", "Hihat", "Kick", "Openhat", "Boom", "Ride", "Snare", 
+"Tom", "Tink"]
+
+// Get parents
 measures = document.querySelector("#measures");
 extras = document.querySelector("#extras");
 
@@ -6,17 +9,35 @@ tempo = 60;
 
 let element;
 
+// Populate measure boxes
 for (i=0;i<4;i++) {
-	element = document.createElement("button")
+	outerDiv = document.createElement("div");
+	outerDiv.classList.add("note-box");
+
+	element = document.createElement("button");
 	element.innerHTML = i + 1;
-	element.classList.add("note-box");
-	measures.appendChild(element);
+	element.classList.add("top")
+
+	soundDiv = document.createElement("div");
+	soundDiv.classList.add("sound-dropdown-box");
+	for (k=0;k<sounds.length;k++) {
+		soundElement = document.createElement("button")
+		soundElement.innerHTML = sounds[k];
+		soundElement.classList.add("sound-box");
+		soundDiv.appendChild(soundElement);
+	}
+	outerDiv.appendChild(element);
+	outerDiv.appendChild(soundDiv);
+	measures.appendChild(outerDiv);
 }
 
+// Populate extras
 element = document.createElement("input")
+element.classList.add("extra");
 extras.appendChild(element);
 
 element = document.createElement("button")
 element.innerHTML = "START";
+element.classList.add("extra");
 extras.appendChild(element);
 
